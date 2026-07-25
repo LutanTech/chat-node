736de9df6a4fa6e0b6c0fb031046e2836c2ab582
LutanTech <lutancorpinfoteam@gmail.com>
2026-07-25 16:55:26 +0300

    Refactor project structure: rename to 'chat-node', update version to 1.0.0, add description and start script; change server port to 9000; enhance vibration functionality in index.html.

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening on port ${PORT}`);
 });
